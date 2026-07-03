85238fb5037001943befa185c4ecf7dc4d5f8442
SanduHans <233099904+sanduni-hansika@users.noreply.github.com>
2026-07-03 17:11:34 +0530

    Set up SwiftUI project

---
+++ Tapfrenzyapp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+ 
+@main
+struct TapFrenzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
